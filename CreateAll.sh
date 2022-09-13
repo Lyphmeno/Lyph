669b25916c4cba80df4f08371437ae6a7719a3af
Lyfmeno <hugolevipro@gmail.com>
2022-09-13 15:51:58 +0200

    Created all the stuff but this is only the first version

---
+++ CreateAll.sh
@@ -0,0 +1,8 @@
+# Adding all the templates files 
+cat TemplateFiles/basicHPP > ~/.templates/.basicHPP
+cat TemplateFiles/basicCPP > ~/.templates/.basicCPP
+cat TemplateFiles/makexc > ~/.templates/.makexc
+cat TemplateFiles/makexcpp > ~/.templates/.makexcpp
+
+# Writting the rc config file 
+cat TemplateFiles/rcTemplate > ~/.zshrc
