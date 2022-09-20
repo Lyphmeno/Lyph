8dcddcc05696851d5f2a33bd8aa7f40b3b54ac38
Lyfmeno <hugolevipro@gmail.com>
2022-09-20 12:45:20 +0200

    Just added gupsh function and testing result

--- CreateAll.sh
+++ CreateAll.sh
@@ -1,3 +1,4 @@
+#!/bin/sh 
 # Adding all the templates files 
 cat TemplateFiles/basicHPP > ~/.templates/.basicHPP
 cat TemplateFiles/basicCPP > ~/.templates/.basicCPP
@@ -5,4 +6,6 @@ cat TemplateFiles/makexc > ~/.templates/.makexc
 cat TemplateFiles/makexcpp > ~/.templates/.makexcpp
 
 # Writting the rc config file 
+unalias -a
 cat TemplateFiles/rcTemplate > ~/.zshrc
+. ~/.zshrc
