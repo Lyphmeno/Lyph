ee5a7564c8c3b43daee34d8c66f61d64cc2bc519
Lyfmeno <hugolevipro@gmail.com>
2023-07-28 17:14:43 +0200

    FUCK NEOVIM IM TIRED OF THIS SHIT

--- CreateAll.sh
+++ CreateAll.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd
+mkdir -p 42 42/Ghub 42/Vogsphere
 mysh=`echo $SHELL | cut -d/ -f3`
 # Creating files
 mkdir -p ~/.templates
@@ -14,21 +16,4 @@ cat TemplateFiles/makexcpp > ~/.templates/.makexcpp
 
 # Writting the rc config file 
 unalias -a
-while true; do
-    read -p "Do you wish to add the brew source ? [Y/N]" yn
-    cat TemplateFiles/rcTemplate > ~/."$mysh"rc
-    case $yn in
-        [Yy]* ) echo ". \$HOME/.brewconfig.zsh" >> ~/."$mysh"rc | echo "Added source for Brew" ; break;;
-        [Nn]* ) echo "No source Added for brew" ; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-while true; do
-    read -p "Do you wish to add an alias for Neovim ? [Y/N]" yn
-    cat TemplateFiles/rcTemplate > ~/."$mysh"rc
-    case $yn in
-        [Yy]* ) echo "alias nvim='~/nvim-linux64/bin/nvim'" >> ~/."$mysh"rc | echo "Added alias for nvim" ; break;;
-        [Nn]* ) echo "No alias added for nvim" ;break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+cat TemplateFiles/rcTemplate > ~/."$mysh"rc
